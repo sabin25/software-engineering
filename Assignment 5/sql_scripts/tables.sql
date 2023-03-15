45211aaf43ea71134c4264a9cca5910bed964ca1
sabin25 <112348895+sabin25@users.noreply.github.com>
2023-03-15 10:10:42 -0400

    assignment 5

---
+++ Assignment 5/sql_scripts/tables.sql
@@ -0,0 +1,9 @@
+-- resources Table:
+CREATE TABLE IF NOT EXISTS resources (
+    id int NOT NULL AUTO_INCREMENT,
+    item varchar(50) NOT NULL,
+    amount int NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- sandwiches table:
